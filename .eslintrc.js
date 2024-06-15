39e53b14299214a15c17956fdb52793742598cd7
hyeseon han <hyeseon405@gmail.com>
2024-06-15 14:21:26 +0900

    feat: prettier 설치 & eslint 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  globals: {
+    JSX: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['/src/assets/**', '/src/styles/**'],
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+  },
+};
