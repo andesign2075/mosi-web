3a92da32ff6044d3cbf764767afe46fbf315631f
hyeseon han <hyeseon405@gmail.com>
2024-06-30 16:28:37 +0900

    feat: Icon component 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
 export { default as HomeIcon } from './HomeIcon';
 export { default as MypageIcon } from './MypageIcon';
 export { default as SmileIcon } from './SmileIcon';
+export { default as KakaoIcon } from './KakaoIcon';
+export { default as AppleIcon } from './AppleIcon';
+export { default as GoogleIcon } from './GoogleIcon';
