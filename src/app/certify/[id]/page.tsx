60f433f36647e1173f088a11c4eb47c7fd31722f
hyeseon han <hyeseon405@gmail.com>
2024-10-28 20:29:59 +0900

    fix: 기존 인증하기 페이지 제거, 인증현황으로만 진행하도록 수정

--- src/app/certify/[id]/page.tsx
+++ src/app/certify/[id]/page.tsx
@@ -1,56 +1,92 @@
 'use client';
 
-import { CertifyCard, ChallengeSummarySection } from '@/components/pages/challenge';
-import { Header, Text } from '@/components/common';
-import { usePathname, useRouter } from 'next/navigation';
+import 'react-calendar/dist/Calendar.css';
 
-import { CheckCircleIcon } from '@/assets/icons';
-import React from 'react';
+import { Header, Text } from '@/components/common';
+import React, { useState } from 'react';
+
+import Calendar from 'react-calendar';
+import { CertifyCard } from '@/components/pages/challenge';
+import { NextPage } from 'next';
+import dayjs from 'dayjs';
 import styles from './certify-detail.module.scss';
-import { useGetChallengeDetail } from '@/queries/challenge';
+import useGetCertifyHistory from '@/queries/review/useGetChallengeReview';
+
+const MOCK_DATA = {
+  progress: 20,
+  certifyCount: 1,
+  failCount: 3,
+  remainCount: 10,
+  startDate: '2024-08-24',
+  endDate: '2024-08-30',
+};
 
 interface Props {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 }
+const CertifyDetailHistory: NextPage<Props> = ({ params }) => {
+  const [value] = useState(dayjs().format());
 
-const CertifyDetail = ({ params }: Props) => {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const { data } = useGetChallengeDetail(params.id);
-  const detailData = data?.data[0];
-
-  if (!detailData) {
-    return null;
-  }
+  const { data } = useGetCertifyHistory({ challengeId: params.id });
 
   return (
     <>
-      <Header variant="light" headerTitle="인증하기" />
+      <Header variant="light" headerTitle="인증현황" />
       <div className={styles.container}>
-        <ChallengeSummarySection
-          thumbnailImageUrl={detailData.thumbnailImageUrl}
-          title={detailData.title}
-          periodWeeks={detailData.periodWeeks}
-          weeklyFrequency={detailData.weeklyFrequency}
-          startDate={detailData.startDate}
-          endDate={detailData.endDate}
-          participantCount={detailData.participantCount}
-        />
-        <div className={styles.divider} />
-        <section className={styles.certify__section}>
-          <div className={styles.certify__section__title__row}>
-            <Text.Title variant={18}>인증(6/14)</Text.Title>
-            <button className={styles.certify__history__button} onClick={() => router.push('/certify/1/history')}>
-              <CheckCircleIcon />
-              <Text.Title variant={14}>인증현황</Text.Title>
-            </button>
+        <section>
+          <div className={styles.title__container}>
+            <Text.Title variant={16}>진행률</Text.Title>
+            <Text.Title variant={24} className={styles.progress}>
+              20%
+            </Text.Title>
           </div>
-          <CertifyCard onClick={() => router.push(`${pathname}/register`)} data={undefined} />
+          <div className={styles.progress__container}>
+            <div className={styles.progress__inner_container} style={{ width: `${MOCK_DATA.progress}%` }} />
+          </div>
+          <div className={styles.info__container}>
+            <div>
+              <Text.Body variant={18}>내 인증 회수</Text.Body>
+              <Text.Title variant={18}>{MOCK_DATA.certifyCount}</Text.Title>
+            </div>
+            <div>
+              <Text.Body variant={18}>실패 회수</Text.Body>
+              <Text.Title variant={18}>{MOCK_DATA.certifyCount}</Text.Title>
+            </div>
+            <div>
+              <Text.Body variant={18}>남은 회수</Text.Body>
+              <Text.Title variant={18}>{MOCK_DATA.certifyCount}</Text.Title>
+            </div>
+          </div>
+          <div>
+            <Calendar
+              locale="ko"
+              formatDay={(_locale, date) => dayjs(date).format('D')}
+              allowPartialRange={false}
+              value={value}
+              calendarType="gregory"
+              showNeighboringMonth={false}
+              next2Label={null} // +1년 & +10년 이동 버튼 숨기기
+              prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
+              onClickYear={() => null}
+              tileClassName={({ date }) => {
+                if (
+                  dayjs(MOCK_DATA.startDate).diff(dayjs(date), 'days') < 0 &&
+                  dayjs(MOCK_DATA.endDate).diff(dayjs(date), 'days') >= 0
+                ) {
+                  return 'calendar__range';
+                }
+              }}
+            />
+          </div>
+        </section>
+        <section className={styles.certify__card__section}>
+          {data?.data.map((ele) => <CertifyCard data={ele} onClick={() => null} />)}
         </section>
       </div>
     </>
   );
 };
 
-export default CertifyDetail;
+export default CertifyDetailHistory;
